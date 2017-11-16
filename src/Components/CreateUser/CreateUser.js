93e4b3f77e3e160aa19ee3561d931d44db98e73c
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-16 15:53:26 -0700

    cloudinary and dropzone working and on state of createuser

--- src/Components/CreateUser/CreateUser.js
+++ src/Components/CreateUser/CreateUser.js
@@ -1,23 +1,102 @@
 import React, { Component } from 'react';
 import './CreateUser.css';
+import { connect } from 'react-redux';
 import profilePlaceholder from '../Nav/profilePlaceholder.png';
-export default class CreateUser extends Component {
+import { getUserInfo } from '../../ducks/reducer.js';
+import { Link } from 'react-router-dom';
+import { Image } from 'cloudinary-react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+
+class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            imageUrl: ''
+        }
+        this.handleDrop = this.handleDrop.bind(this);
+    }
+    handleDrop = files => {
+        // Push all the axios request promise into a single array
+        const uploaders = files.map(file => {
+            // Initial FormData
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("tags", `codeinfuse, medium, gist`);
+            formData.append("upload_preset", "kgjwyzim"); // Replace the preset name with your own
+            formData.append("api_key", 361944559931919); // Replace API key with your own Cloudinary key
+            formData.append("timestamp", (Date.now() / 1000) | 0);
+            const cloudName = 'dfkw5isvi';
+            const url = "https://api.cloudinary.com/v1_1/" + cloudName + '/image/upload'
+            // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+            return axios.post("https://api.cloudinary.com/v1_1/dfkw5isvi/image/upload", formData, {
+                headers: { "X-Requested-With": "XMLHttpRequest" },
+            }).then(response => {
+                const data = response.data;
+                const fileURL = data.secure_url // You should store this URL for future references in your app
+                // console.log(data);
+                axios.get(data.secure_url).then(res => {
+                    console.log(res);
+                    this.setState({
+                        imageUrl: res.config.url
+                    })
+                })
+            })
+
+
+        })
+        // Once all the files are uploaded 
+        axios.all(uploaders).then(() => {
+            // ... perform after upload is successful operation
+
+        });
+    }
+
+    componentDidMount() {
+
+    }
     render() {
+
+        // DROPZONE-------------------------------------------------------
+
+
+        const user = this.props.user;
         return (
             <div className='createuser'>
                 <div className='createuserHolder'>
                     <h1>Welcome to W I P</h1>
-                    
+
                     {/* left side of info -- profile pic */}
+
+
+
+
                     <div className='createuserInfoHolder'>
                         <div className='infoItem profileImgDiv'>
-                            <img src={profilePlaceholder} alt='profileimg' />
+                            {/*===| CLOUDINARY |=================================*/}
+                            
+
+                            {/* DROPZONE */}
+                            <Dropzone
+                                onDrop={this.handleDrop}
+                                multiple
+                                accept="image/*"
+                                style={'border:none'}
+                            >
+                                <img src={this.state.imageUrl ? this.state.imageUrl : profilePlaceholder} alt='profileimg' />
+    
+                            </Dropzone>
+
+                            <span>Change image</span>
                         </div>
 
-                    {/* right side of info */}
+
+                        {/*===| RIGHT SIDE OF INFO |=================================*/}
                         <div className='userInfo infoItem'>
                             <h3>Name</h3>
-                            <input type='text' placeholder='reduxUsername' value='' />
+                            {user.id ? user.first_name + ' ' + user.last_name : 'first and last'}
+                            <input type='text' placeholder='' value='' />
                             <h3>Email</h3>
                             <input type='text' placeholder='email' value='' />
                             <h3>Highschool</h3>
@@ -31,6 +110,8 @@ export default class CreateUser extends Component {
                         </div>
 
                     </div>
+                    <Link to={'/assessment'}><div className='createUserNextBtn'><h3> Submit </h3></div></Link>
+
 
 
                 </div>
@@ -40,3 +121,10 @@ export default class CreateUser extends Component {
         )
     }
 }
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, { getUserInfo })(CreateUser);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
 import { unregister } from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
