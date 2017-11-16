059b79c23afe3f93b8198bd7e43d582385416c9e
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-11-16 16:17:24 -0700

    glassdoor works

--- server/controllers/glassdoorController.js
+++ server/controllers/glassdoorController.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 module.exports = {
     getJobs: (req,res,next) => {
-        return axios.get(`http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${ process.env.REACT_APP_GLASSDOOR_PARTNER_ID }&t.k=${ process.env.REACT_APP_GLASSDOOR_KEY }&action=employers&q=pharmaceuticals&userip=localhost:3000&useragent=Mozilla/%2F4.0`).then(response => {
+        return axios.get(`http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${ process.env.REACT_APP_GLASSDOOR_PARTNER_ID }&t.k=${ process.env.REACT_APP_GLASSDOOR_KEY }&action=jobs-stats&“&countryId=1&jobTitle=all&userip=localhost:3000&useragent=Mozilla/%2F4.0`).then(response => {
     console.log(response.data);
     res.status(200).send(response.data);
     })
--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.get('/auth/logout', (req, res) => {
 
 //--------------GLASSDOOR--------------//
 
-app.get(`http://api.glassdoor.com/api/api.htm?t.p=${ process.env.REACT_APP_GLASSDOOR_PARTNER_ID }&t.k=${ process.env.REACT_APP_GLASSDOOR_KEY }&userip=localhost:3000&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=11`, gdc.getJobs)
+app.get(`http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${ process.env.REACT_APP_GLASSDOOR_PARTNER_ID }&t.k=${ process.env.REACT_APP_GLASSDOOR_KEY }&action=jobs-stats&“&countryId=1&jobTitle=all&userip=localhost:3000&useragent=Mozilla/%2F4.0`, gdc.getJobs)
 
 
 //----------COLLEGES TO DB------------//
