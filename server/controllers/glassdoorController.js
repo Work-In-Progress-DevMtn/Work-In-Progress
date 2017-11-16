415f01f7b50f7594592947d08c4f712631d705e8
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-11-16 09:25:50 -0700

    glassdoor api

--- server/controllers/glassdoorController.js
+++ server/controllers/glassdoorController.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const axios = require('axios');
+
 
 module.exports = {
     getJobs: (req,res,next) => {
-        return axios.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=120&t.k=fz6JLNDfgVs&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0').then(response => {
+        return axios.get(`http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${process.env.GLASSDOOR_PARTNER_ID}&t.k=${process.env.GLASSDOOR_KEY}&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0`).then(response => {
     console.log(response.data);
     res.status(200).send(response.data);
     })
