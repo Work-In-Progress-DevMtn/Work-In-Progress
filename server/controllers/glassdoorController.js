a1d79f5d7211790dcbea5da9ae5043c23ed1b18d
Ryan Parke <ryanjparke94@gmail.com>
2017-11-16 11:27:15 -0700

    progress on glasdoor api

--- server/controllers/glassdoorController.js
+++ server/controllers/glassdoorController.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = {
     getJobs: (req,res,next) => {
-        return axios.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=120&t.k=fz6JLNDfgVs&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0').then(response => {
+        return axios.get(`http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${process.env.REACT_APP_PARTNER_ID}&t.k=${process.env.REACT_APP_GLASSDOOR_KEY}&action=employers&q=pharmaceuticals&userip=localhost:3000&useragent=Mozilla/%2F4.0`).then(response => {
     console.log(response.data);
     res.status(200).send(response.data);
     })
