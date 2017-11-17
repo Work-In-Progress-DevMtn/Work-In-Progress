93ab356a98fc59bd1b1cb5a35688a443ec91798f
Josi Moore <josib.moore@gmail.com>
2017-11-17 14:46:11 -0700

    db changes -- working on favorites

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -1,5 +1,10 @@
 module.exports = {
     addCollege: (req, res) => {
-        
+        const db = req.app.get('db');
+        const { id, user } = req.params;
+
+        db.add_fave_college( [id, user] ).then( favorites => {
+            res.send(favorites);
+        } )
     }
 }
