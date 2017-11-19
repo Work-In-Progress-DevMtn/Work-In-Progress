38431f9c75981d6de4ffc5ab85de5d5c4a690e37
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-19 01:47:07 -0700

    edit profile working fully now need profile to change from edited info

--- src/Components/CreateUser/CreateUser.js
+++ src/Components/CreateUser/CreateUser.js
@@ -15,12 +15,13 @@ class CreateUser extends Component {
 
         this.state = {
             imageUrl: '',
-            fullName: '',
-            email: '',
+            firstName: '',
+            lastName: '',
+            myEmail: '',
             highschool: '',
             currentYear: '',
             city: '',
-            state: ''
+            USstate: ''
         }
         this.handleDrop = this.handleDrop.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -62,7 +63,21 @@ class CreateUser extends Component {
     }
 
     componentDidMount() {
-        this.props.getUserInfo();
+        this.props.getUserInfo().then(() => {
+            if (user.id) {
+                this.setState({
+                    firstName: user.first_name,
+                    lastName: user.last_name,
+                    myEmail: user.email,
+                    highschool: user.high_school,
+                    currentYear: user.current_year,
+                    city: user.location_city,
+                    USstate: user.location_state
+                }, () => console.log('mountedState: ', this.state))
+            }    
+        });
+        const user = this.props.user;
+        
     }
 
     handleChange(prop, val) {
@@ -92,18 +107,33 @@ class CreateUser extends Component {
             <div>
                 <TextField
                     hintText=''
+
+                    // user.id ? user.first_name :
+                    // user.id ? user.last_name : 
+                    // user.id ? user.email : 
+                    // user.id ? user.high_school : 
+                    // user.id ? user.current_year :
+                    // user.id ? user.location_city :
+                    // user.id ? user.location_state :
                     // hintText={user.id ? user.first_name + ' ' + user.last_name : this.state.fullName ? this.state.fullName : 'first and last'}
-                    floatingLabelText="Name"
-                    value={user.id ? user.first_name + ' ' + user.last_name : this.state.firstName}
-                    onChange={(e) => this.handleChange('fullName', e.target.value)}
-                    style={{ width: 200 }}
+                    floatingLabelText="First name"
+                    value={ this.state.firstName ? this.state.firstName : ''}
+                    onChange={(e) => this.handleChange('firstName', e.target.value)}
+                    style={{ width: 95}}
+                /> <TextField
+                    hintText=''
+                    // hintText={user.id ? user.first_name + ' ' + user.last_name : this.state.fullName ? this.state.fullName : 'first and last'}
+                    floatingLabelText="Last name"
+                    value={this.state.lastName ? this.state.lastName : ''}
+                    onChange={(e) => this.handleChange('lastName', e.target.value)}
+                    style={{ width: 95 }}
                 /> <br />
 
                 <TextField
                     hintText="example@gmail.com"
                     floatingLabelText="Email"
-                    value={user.id ? user.email : this.state.email}
-                    onChange={(e) => this.handleChange('email', e.target.value)}
+                    value={this.state.myEmail ? this.state.myEmail : ''}
+                    onChange={(e) => this.handleChange('myEmail', e.target.value)}
                     style={{ width: 200 }}
                 /> <br />
             </div>
@@ -115,29 +145,29 @@ class CreateUser extends Component {
                 <TextField
                     hintText=""
                     floatingLabelText="Highschool"
-                    value={user.id ? user.high_school : this.state.highschool}
+                    value={this.state.highschool ? this.state.highschool : ''}
                     onChange={(e) => this.handleChange('highschool', e.target.value)}
                     style={{ width: 200 }}
                 /><br />
                 <TextField
                     hintText=""
                     floatingLabelText="Current year"
-                    value={user.id ? user.current_year : this.state.currentYear}
+                    value={this.state.currentYear ? this.state.currentYear : ''}
                     onChange={(e) => this.handleChange('currentYear', e.target.value)}
                     style={{ width: 200 }}
                 /><br />
                 <TextField
                     hintText=""
                     floatingLabelText="City"
-                    value={user.id ? user.location_city : this.state.city}
+                    value={ this.state.city ? this.state.city : ''}
                     onChange={(e) => this.handleChange('city', e.target.value)}
                     style={{ width: 200 }}
                 /><br />
                 <TextField
                     hintText=""
                     floatingLabelText="State"
-                    value={user.id ? user.location_state : this.state.state}
-                    onChange={(e) => this.handleChange('state', e.target.value)}
+                    value={ this.state.USstate ? this.state.USstate : ''}
+                    onChange={(e) => this.handleChange('USstate', e.target.value)}
                     style={{ width: 200 }}
                 /><br />
             </div>
