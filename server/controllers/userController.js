3101d489b977a4168acebdf63dc5b2b2931593af
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-27 12:41:36 -0700

    about me section finished with edit/save/cancel buttons

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
         const user = req.body;
         const userId = req.params.id;
 
-        db.update_user([userId, user.firstName, user.lastName, user.myEmail, user.highschool, user.currentYear, user.city, user.USstate, user.imgUrl, false] ).then( user => {
+        db.update_user([userId, user.firstName, user.lastName, user.myEmail, user.highschool, user.currentYear, user.city, user.USstate, user.imgUrl, false, user.about] ).then( user => {
             res.send(user);
         })
     }
