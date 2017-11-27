5e5e4c39993283313bd0a8c34aad4dcf5f127b82
Ryan Parke <ryanjparke94@gmail.com>
2017-11-27 16:22:07 -0700

    work on footer and new packages in json

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import './Footer.css';
- 
-class Footer extends Component{ 
-    render(){
+
+
+class Footer extends Component {
+    render() {
         return (
             <div className='Footer'>
-                <div className='footerItemsHolder'>
-                    <div className='Links footerSection'>    
-                        <a>Link</a>
-                        <a>Link</a>
-                        <a>Link</a>
+                {/* ****************LEFT SECTION******************************* */}
+                <div className='leftSection'>
+                    <div className='leftTitle'>
+                        <h2>CONNECT WITH US</h2>
                     </div>
-                    <div className='more footerSection'> more</div>    
-                    <div className='extra stuff footerSection'> extra</div>    
-                </div>    
+                    <div className='leftContent'>
+                        <a href='https://www.linkedin.com/'>LinkedInIcon</a>
+                        <a href='https://www.instagram.com/'>InstaIcon</a>
+                        <a href='https://www.facebook.com/'>FacebookIcon</a>
+                    </div>
+                </div>
+                {/* ****************MIDDLE SECTION******************************* */}
+                <div className='midSection'>
+                    <div className='midTitle'>
+                        <h2>COMMITTED TO YOUR PROGRESS</h2>
+                    </div>
+                    <div className='midContent'>
+                        <p>The transition between high school and college, employment and career choice is continually becoming more and more difficult.</p>
+                        <p>W.I.P. is designed to create a smooth pathway toward reaching individual goals by providing crucial information and tools to make progress toward your future.</p>
+
+                    </div>
+                </div>
+                {/* ****************RIGHT SECTION******************************* */}
+
+
             </div>
         )
     }
 }
 export default Footer;
+
+
+
+
+
+
+{/* <p>The transition between high school and college, employment and career choice is continually becoming more and more difficult.
+</p>
+<p>W.I.P. is designed to create a smooth pathway toward reaching individual goals by providing crucial information and tools to make progress toward your future.
+    </p> */}
