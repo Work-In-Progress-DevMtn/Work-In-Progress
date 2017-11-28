25c0569165fdf6ccf046c064a27a181278cd6206
Josi Moore <josib.moore@gmail.com>
2017-11-28 13:51:23 -0700

    updated css in studentprofile

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Admin.css';
 import axios from 'axios';
 import RemoveIcon from './RemoveIcon';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 class Admin extends Component {
@@ -46,13 +46,13 @@ class Admin extends Component {
     handleSearch() {
         if (this.state.highSchool && this.state.studentName) {
             return this.searchBySchoolAndName(this.state.highSchool, this.state.studentName);
-        } 
+        }
         else if (this.state.highSchool && !this.state.studentName) {
             return this.searchBySchool(this.state.highSchool);
         }
         else if (this.state.studentName && !this.state.highSchool) {
             return this.searchByName(this.state.studentName);
-        } 
+        }
         else {
             alert('No Results')
         }
@@ -60,39 +60,39 @@ class Admin extends Component {
 
     searchBySchoolAndName(school, name) {
         axios.get(`/getstudentbyschoolandname/${school}/${name}`)
-             .then( res => {
-                 this.setState({
-                     students: res.data
-                 })
-             })
+            .then(res => {
+                this.setState({
+                    students: res.data
+                })
+            })
     };
 
     searchBySchool(school) {
         axios.get(`/getstudentbyschool/${school}`)
-             .then( res => {
-                 this.setState({
-                     students: res.data
-                 })
-             })
+            .then(res => {
+                this.setState({
+                    students: res.data
+                })
+            })
     };
 
     searchByName(name) {
         axios.get(`/getstudentbyname/${name}`)
-             .then( res => {
-                 this.setState({
-                     students: res.data
-                 })
-             })
+            .then(res => {
+                this.setState({
+                    students: res.data
+                })
+            })
     };
 
     removeStudent(id) {
         axios.delete(`/removestudent/${id}`)
-             .then( axios.get('/getallstudents').then(res => {
-                    this.setState({
-                        students: res.data
-                    })
+            .then(axios.get('/getallstudents').then(res => {
+                this.setState({
+                    students: res.data
                 })
-              )
+            })
+            )
     }
 
 
@@ -102,19 +102,22 @@ class Admin extends Component {
         const studentList = this.state.students.map((student, i) => {
             return <div key={i} className='student_list'>
 
-                <Link to={`/profile/${student.id}`} target='_blank'>
+
                 <div className='student'>
 
                     <div className='image_container'>
-                        <div onClick={ () => this.removeStudent(student.id)} className='remove_button'><RemoveIcon /></div>
+                        <div onClick={() => this.removeStudent(student.id)} className='remove_button'><RemoveIcon /></div>
                         <img src={student.img_url} alt='' className='student_image' />
                     </div>
-                    <span className='student_name'>{student.first_name} {student.last_name}</span>
-                    <span className='student_hs'>{student.current_year}, {student.high_school}</span>
+                    
+                    <Link to={`/profile/${student.id}`} target='_blank' className='link'>
+                        <span className='student_name'>{student.first_name} {student.last_name}</span> 
+                    </Link>
+                        <span className='student_hs'>{student.current_year}, {student.high_school}</span>
                     
                 </div>
-                
-                </Link>
+
+
 
             </div>
         })
@@ -126,14 +129,14 @@ class Admin extends Component {
                         <span className='admin_header'>Students</span>
 
                         <div className='search_container'>
-                            <span>High School: </span><input className='search' onChange={ e => this.updateHighSchool(e.target.value) }/>
+                            <span>High School: </span><input className='search' onChange={e => this.updateHighSchool(e.target.value)} />
                         </div>
 
                         <div className='search_container'>
-                            <span>Student Last Name: </span><input className='search' onChange={ e => this.updateName(e.target.value) } />
+                            <span>Student Last Name: </span><input className='search' onChange={e => this.updateName(e.target.value)} />
                         </div>
 
-                        <button className='admin_search' onClick={ this.handleSearch }> Search </button>
+                        <button className='admin_search' onClick={this.handleSearch}> Search </button>
                     </div>
                 </div>
 
